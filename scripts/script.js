7e2eebbc3d869e8b9ac45ed3c492e4c2024ee9d7
codinghaytam <haytams2210@gmail.com>
2024-01-13 01:48:50 +0100

    error fix in operator function and adding input handler: - function operator line 15: making tospliced depandente of the if statements - input handler: function that gets an input as string and returns an array of strings that can be used by the operation function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 //a function that separates the elements of a string to identify the operation and give the resulte
 function operation(text)
 {
-    let s ;
+    console.log(text);
     for ( i = 0 ; i < (text.length - 1) ; i++ )
     {
         if (text[i]==="(")
@@ -12,19 +12,23 @@ function operation(text)
             text=text.toSpliced(i+1,j-i);
         }
     }
+    console.log(text);
     for ( i = 0 ; i <= (text.length - 1) ; i++ ) 
     {
         if ( text [i] == "*" )
             {
                 text[i-1] = String( Number(text[i-1]) * Number(text[i+1]) );
+                text=text.toSpliced(i,2);   
             }
         if(text [i] == "/" )
             {
                 text[i-1]=String( Number(text[i-1]) / Number(text[i+1]) );
+                text=text.toSpliced(i,2);   
             }
-        text=text.toSpliced(i,2);   
     }
-    s = Number(text[0])
+    console.log(text);
+
+    let s = Number(text[0]);
     for( i = 0 ; i <= (text.length-1) ; i+=2 ) 
         { 
 
@@ -37,13 +41,28 @@ function operation(text)
                     s-= Number(text[i+2]) ;
                     break;
             }
+
         }
     return s;
 }
 
+function input_handler(input)
+{
+    let symboles="+-/*()";
+    input = input.trim();
+    input = input.replaceAll(" ","");
+    for ( i = 0; i < input.length ; i++ )
+    {
+        if(isNaN(Number(input[i])) && symboles.indexOf(input[i])==-1) return "Syntax ERROR";
+        if(symboles.indexOf(input[i])>=0 && symboles.indexOf(input[i+1])>=0) return "Syntax ERROR";
+    }
+    if(symboles.indexOf(input[0])>=0 || symboles.indexOf(input[input.length-1])>=0) return "Syntax ERROR";
+    for( i = 0; i < (symboles.length); i++)
+    {
+        if(symboles)
+        input = input.replaceAll(symboles[i]," "+symboles[i]+" ");
+    }
+    input = input.split(" ");
+    return input;
+}
 
-let input = prompt();
-input=input.trim();
-input = input.split(" ");
-console.log(input)
-console.log(operation(input));
